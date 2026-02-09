123498f0b04f8905064f0fec97a02d94143dc04d
Prateek Chandra <pchandra@griddynamics.com>
2026-02-09 12:05:35 +0530

    codes

---
+++ MaximumSubarray.py
@@ -0,0 +1,23 @@
+def max_subarray(nums):
+    maxSum = float('-inf')
+    currentSum = 0
+
+    for num in nums:
+        currentSum += num
+
+        if currentSum > maxSum:
+            maxSum = currentSum
+
+        if currentSum < 0:
+            currentSum = 0
+
+    return maxSum
+
+
+def main():
+    nums = list(map(int, input("Enter numbers separated by space: ").split()))
+    print(max_subarray(nums))
+
+
+if __name__ == "__main__":
+    main()
